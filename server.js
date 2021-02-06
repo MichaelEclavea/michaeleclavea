0733309fe9763ed1497006a7681fee583b358ac6
MichaelEclavea <>
2021-02-06 02:45:42 -0800

    changes server routes, index and css routes

--- server.js
+++ server.js
@@ -2,22 +2,31 @@
 
 const express = require('express');
 const app = express();
-const path = require('path');
-app.use(express.static(__dirname + '/public'));
+// const path = require('path');
+// app.use(express.static(__dirname + '/public'));
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/index.html'));
+// });
+
+// app.get('/projects', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/projects.html'));
+// });
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/about.html'));
+// });
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
-});
+    res.send('/index.html')
+})
 
 app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname+'/projects.html'));
-});
+    res.send('/projects.html')
+})
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname+'/about.html'));
-});
-
-
-
+    res.send('/about.html')
+})
 
 app.listen(3000, (req, res) => {
     console.log('App is listening on port 3000')
